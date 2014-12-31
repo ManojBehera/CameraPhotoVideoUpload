cba05af50ee5d8550a613d0f79854bccc2905832
Manoj Behera <mnj.behera1@gmail.com>
2014-12-31 12:25:06 +0530

    made changes

---
+++ CameraPhotoVideoUpload/gen/com/samlei/cameravideoupload/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.samlei.cameravideoupload;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CameraPhotoVideoUpload/src/com/samlei/cameravideoupload/Config.java
@@ -0,0 +1,9 @@
+package com.samlei.cameravideoupload;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = "http://192.168.0.104/AndroidFileUpload/fileUpload.php";
+	
+	// Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
